f9d5b4abca13ac984dfb031134f793735148985c
cenyaoyi <cenyaoyi@163.com>
2013-10-16 17:59:29 +0800

    Signed-off-by: cenyaoyi <cenyaoyi@163.com>

---
+++ src/cn/sh/mhedu/mhzx/schoolboard/MainActivity.java
@@ -0,0 +1,50 @@
+package cn.sh.mhedu.mhzx.schoolboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+	private WebView mWebView;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		initView();
+	}
+	
+	private void initView() {
+		mWebView = (WebView) findViewById(R.id.webview);
+		mWebView.setWebViewClient(new MyWebViewClient());
+		mWebView.getSettings().setJavaScriptEnabled(true);
+		mWebView.loadUrl("http://mhzx.mhedu.sh.cn");
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	private class MyWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+	    if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+	    	mWebView.goBack();
+	        return true;
+	    } 
+		return super.onKeyDown(keyCode, event);
+	}
+
+}
